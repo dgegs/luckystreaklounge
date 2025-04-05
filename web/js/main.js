b226d8aad09a22ea2dfe96956db55c85e028986a
dgegs <124631423+dgegs@users.noreply.github.com>
2025-04-05 00:54:35 -0500

    photo page work

--- web/js/main.js
+++ web/js/main.js
@@ -1,31 +1,41 @@
-function DisplayCountDown(){
-// Set the date we're counting down to
-var countDownDate = new Date("Mar 21, 2025 15:37:25").getTime();
+const imagePaths = [
+  "images/frontOld.jpg",
+  "images/entranceOldThumb.jpg",
+  "images/entranceMain.jpg",
+  "images/ceilingOld.jpg",
+  "images/ceilingBackView.jpg",
+  "images/ceilingOldFront.jpg",
+  "images/backSideWall.jpg",
+  "images/gamingRoom.jpg",
+  "images/coffeeRoom.jpg",
+  "images/backHall.jpg",
+  "images/paintCeilingFrontMid.jpg",
+  "images/ceilingFrontPainted.jpg",
+  "images/ceilingMidPainted.jpg",
+  "images/ceilingBackPainted.jpg",
+  "images/frontDark.jpg",
+  "images/griffinPainting.jpg",
+  "images/frontFloorPadding.jpg",
+  "images/frontFlooring2.jpg",
+  "images/finalFloorPiece.jpg",
+  "images/frontFloorLookBackFinished.jpg",
+  "images/frontViewFinished.jpg",
+  "images/carpetPrep.jpg",
+  "images/mainWallWrap.jpg",
+  "images/veagaWallWrap.jpg",
+  "images/carpetInstalled.jpg",
+  "images/tables.jpg",
+  "images/TheFrontBar.jpg",
+  "images/gamesLeft.jpg",
+  "images/gamesRight.jpg",
+  "images/windowSign.jpg"
 
-// Update the count down every 1 second
-var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+  
+  
+];
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-}
-DisplayCountDown();
+imagePaths.forEach((src) => {
+  const img = new Image();
+  img.src = src;
+});
